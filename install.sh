a67b5b291b5fec97c852d162031ceb554149b2dd
Diego Nogueira <diegoalvareznogueira@gmail.com>
2018-06-02 21:36:14 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,9 +19,10 @@ ln -sf ~/.vim.d/vimrc.vim ~/.vimrc
 mkdir -p ~/.vim/bundle
 
 # clone vundle plugin
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    
 # install all plugins
-vim +PluginInstall +qall
+vim +PlugInstall +qall
 
 echo 'Instaled!'
